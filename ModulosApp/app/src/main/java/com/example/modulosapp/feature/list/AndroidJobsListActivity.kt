99d2fcdd6905a48e31450c251f4b5b2acddda460
gabrielalima558 <gabrielalima558@gmail.com>
2019-09-22 07:49:08 -0300

    Permissao de internet

--- ModulosApp/app/src/main/java/com/example/modulosapp/feature/list/AndroidJobsListActivity.kt
+++ ModulosApp/app/src/main/java/com/example/modulosapp/feature/list/AndroidJobsListActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.modulosapp.R
